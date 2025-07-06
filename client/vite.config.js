1e506d218d464fc724258ad332d62c4be88b3b39
KAKA12-19 <evenezertadesse@gmail.com>
2025-07-06 15:35:56 +0300

    some changes in vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./", // ✅ use './' for Netlify
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    manifest: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name].[hash][extname]",
+        entryFileNames: "assets/[name].[hash].js",
+      },
+    },
+  },
+});
